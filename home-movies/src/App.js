29c8179a23a3336e7893d60a0d0458929285847d
Bram Howard <bram.howard@gmail.com>
2017-01-05 15:20:59 -0600

    Some basic functionality.

--- home-movies/src/App.js
+++ home-movies/src/App.js
@@ -1,21 +1,91 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+
+class MovieList extends Component {
+  constructor() {
+    super();
+    this.state = {
+      movies: Array,
+      addMode: false
+    };
+  }
+
+  render() {
+    let addForm;
+
+    if(this.state.addMode)
+      addForm = <AddForm onSubmit={() => this.submitForm()} />;
+    else
+      addForm = null;
+
+    return  (
+      <div>
+        <h1>My Movies</h1>
+        <div className="movie-list">
+          {this.renderTable()}
+        </div>
+        <AddDeleteUpdate onAdd={() => this.showAddForm()} />
+        {addForm}
+      </div>
+    );
+  }
+
+  renderTable() {
+    return (
+      <table>
+        <tr>
+          <th>Title</th>
+          <th>Genre</th>
+          <th>Year</th>
+          <th>Rating</th>
+        </tr>
+      </table>
+    );
+  }
+
+  showAddForm() {
+    this.setState({
+      movies: Array,
+      addMode: true
+    });
+  }
+
+  submitForm() {
+    this.setState({
+      movies: Array,
+      addMode: false
+    });
+  }
+}
+
+class AddDeleteUpdate extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <button className="Add" onClick={() => this.props.onAdd()}>Add</button>
+        <button className="Delete">Delete</button>
+        <button className="Update">Update</button>
       </div>
     );
   }
 }
 
-export default App;
+class AddForm extends Component {
+  render() {
+    return (
+      <div>
+        <ul>
+          <li>Title: <input type="text" name="title">{this.props.title}</input></li>
+          <li>Genre: <input type="text" name="genre">{this.props.genre}</input></li>
+          <li>Year: <input type="text" name="year">{this.props.year}</input></li>
+          <li>Rating: <input type="number" min="0" step="0.1" name="rating">{this.props.rating}</input></li>
+        </ul>
+        <button className="Submit" onClick={() => this.props.onSubmit()}>Submit</button>
+      </div>
+    );
+  }
+}
+
+
+export default MovieList;
